21170632f364a88d878550922359371f8873b4ac
CesarRepollet <114613615+CesarRepollet@users.noreply.github.com>
2023-05-19 15:47:41 +0200

    Actualización

--- liga/src/Jugador.java
+++ liga/src/Jugador.java
@@ -1,10 +1,24 @@
 public class Jugador {
     private String nombre;
+    private int edad;
     private int numero;
 
-    public Jugador(String nombre, int numero) {
+    private int goles;
+
+    private int asistencias;
+
+    private int tarjetas_amarillas;
+
+    private int tarjetas_rojas;
+
+    public Jugador(String nombre, int edad, int numero, int goles, int asistencias, int tarjetas_amarillas, int tarjetas_rojas) {
         this.nombre = nombre;
+        this.edad = edad;
         this.numero = numero;
+        this.goles = goles;
+        this.asistencias = asistencias;
+        this.tarjetas_amarillas = tarjetas_amarillas;
+        this.tarjetas_rojas = tarjetas_rojas;
     }
 
     public Jugador() {
@@ -12,6 +26,14 @@ public class Jugador {
 
     public String getNombre() {
         return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     public void setNombre(String nombre) {
@@ -26,12 +48,48 @@ public class Jugador {
         this.numero = numero;
     }
 
-    @Override
-    public String toString() {
-        return "Jugadores{" +
-                "nombre='" + nombre + '\'' +
-                ", numero=" + numero +
-                '}';
+    public int getGoles() {
+        return goles;
     }
 
+    public void setGoles(int goles) {
+        this.goles = goles;
+    }
+
+    public int getAsistencias() {
+        return asistencias;
+    }
+
+    public void setAsistencias(int asistencias) {
+        this.asistencias = asistencias;
+    }
+
+    public int getTarjetas_amarillas() {
+        return tarjetas_amarillas;
+    }
+
+    public void setTarjetas_amarillas(int tarjetas_amarillas) {
+        this.tarjetas_amarillas = tarjetas_amarillas;
+    }
+
+    public int getTarjetas_rojas() {
+        return tarjetas_rojas;
+    }
+
+    public void setTarjetas_rojas(int tarjetas_rojas) {
+        this.tarjetas_rojas = tarjetas_rojas;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", numero=" + numero +
+                ", goles=" + goles +
+                ", asistencias=" + asistencias +
+                ", tarjetas_amarillas=" + tarjetas_amarillas +
+                ", tarjetas_rojas=" + tarjetas_rojas +
+                '}';
+    }
 }
--- liga/src/Main.java
+++ liga/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Equipo equipo2 = new Equipo(0,"Real Madrid");
         liga1.getEquipos().add(equipo1);
         liga1.getEquipos().add(equipo2);
-        Jugador jugador1 = new Jugador("cesar", 1);
+        Jugador jugador1 = new Jugador("Lionel Andres Messi", 10, 30, );
         equipo1.getJugadores().add(jugador1);
         System.out.println(liga1);
     }
